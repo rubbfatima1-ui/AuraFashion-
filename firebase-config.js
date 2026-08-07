4dec7625eafba83a0f7acd0ba863b0b394f00579
rubbfatima1-ui <rubbfatima1@gmail.com>
2026-08-07 10:16:37 +0500

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase Configuration
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Firebase initialize karne ke liye
+// Firebase SDK add karne ke baad use hoga
+
+console.log("Firebase Config Loaded");
